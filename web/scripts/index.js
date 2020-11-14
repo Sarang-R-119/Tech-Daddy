f839bed91f42db9d08372235683b233f37ce041f
Sarang-R-119 <sarangvr@yahoo.com>
2020-11-15 02:09:15 +0400

    Changed the index.html, created auth.js & index.js

---
+++ web/scripts/index.js
@@ -0,0 +1,16 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function openForm_signup()
+{
+    document.getElementById("form-signup").style.display = "block";
+}
+
+function closeForm_signup() {
+    document.getElementById("form-signup").style.display = "none";
+}
