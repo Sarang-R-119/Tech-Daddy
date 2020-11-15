8f71ed957ca250917a84e0536b0fae12b336e652
Mahir92 <asefkabir92@gmail.com>
2020-11-15 09:57:31 -0500

    firestore-buddy: Upload scrapped data from file to firestore

---
+++ firestore-buddy/main.py
@@ -0,0 +1,33 @@
+from threading import main_thread
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import glob
+import json
+
+
+def read_jsons(path):
+    """Read files from a directory"""
+    return glob.glob(path)
+
+
+def get_content(filepath):
+    """Read json from a file"""
+    f = open(filepath, "r")
+    content = json.load(f)
+    f.close()
+    return content
+
+
+if __name__ == "__main__":
+    cred = credentials.Certificate("firebase-sdk.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+    json_products = read_jsons("../scrapper/json/*.json")
+
+    for json_product in json_products:
+        content = get_content(json_product)
+
+        doc_ref = db.collection('devices').document(content["id"])
+        doc_ref.set(content)
