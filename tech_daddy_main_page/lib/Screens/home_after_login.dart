2b8dad0623e2a81e9c6ac66dc969adf9aba4fe57
DanielSabanov <44280274+DanielSabanov@users.noreply.github.com>
2021-01-23 02:29:43 -0500

    Add login screen and navigation between home and login screen.

--- tech_daddy_main_page/lib/Screens/home_after_login.dart
+++ tech_daddy_main_page/lib/Screens/home_after_login.dart
@@ -1,47 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(TechDaddy());
-
-class TechDaddy extends StatelessWidget {
-  final appTitle = 'Drawer Demo';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appTitle,
-      home: HomePagePostLogin(title: appTitle, user: "user"),
-    );
-  }
-}
-
-class HomePageBeforeLogin extends StatelessWidget {
-  final String title;
-  final String user;
-
-  HomePageBeforeLogin({Key key, this.title, this.user}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Color(0xff821c34),
-          title: Row(
-            children: <Widget>[
-              Image.asset(
-                'assets/logo.png',
-                height: 55,
-                width: 55,
-              ),
-              Spacer(flex: 2),
-              RaisedButton(child: Text('Login / Sign up'), color: Color(0xffd0532b), onPressed: () {},)
-            ],
-            mainAxisAlignment: MainAxisAlignment.start,
-          )),
-      backgroundColor: Color(0xfff6dad1),
-    );
-  }
-}
-
 class HomePagePostLogin extends StatelessWidget {
   final String title;
   final String user;
@@ -65,13 +23,13 @@ class HomePagePostLogin extends StatelessWidget {
           children: <Widget>[
             new Container(
               color: Color(0xfff6dad1),
-              child: new Image.asset('assets/icon1.ico'),
+              child: new Image.asset('assets/icon1.png'),
               width: 200,
               height: 200,
             ),
             new Container(
               color: Color(0xfff6dad1),
-              child: new Image.asset('assets/icon1.ico'),
+              child: new Image.asset('assets/icon1.png'),
               width: 200,
               height: 200,
             ),
