ce01fa3c762eed09c20bdac65c5e3232f3ccf76f
Nate <dark7storm@pop-os.localdomain>
2021-04-04 12:28:06 -0400

    Styled device-list.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,36 @@
+// onClick functions to open and close form popup
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+    $('#myForm').css('display', "block");
 }
-
 function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+    $('#myForm').css('display', "none");
 }
-
-// function openForm_signup()
-// {
-//     document.getElementById("form-signup").style.display = "block";
-// }
-
-// function closeForm_signup() {
-//     document.getElementById("form-signup").style.display = "none";
-// }
 
 var el = document.getElementById("menu-button");
-        var currState = false;
-        el.addEventListener("click", function(){
-            currState = !currState;
-            var popup = document.getElementById("myForm");
-            popupStyle = window.getComputedStyle(popup),
-            displayS = popupStyle.display;
-            if (displayS == 'block') {document.getElementById("myForm").style.display = "none";}
-            el.style.backgroundImage=(currState ? "url(./assets/images/back.png)":"url(./assets/images/menu.png)");
-            // document.getElementById("body").style.backgroundColor=(currState ? "#ed6d1e":"white");
-            var temp1 = document.getElementsByClassName("content1");
-            for ( i = 0; i < temp1.length; i++ ) {
-                temp1[i].style.display=(currState ? "none":"block");
-            }
-            document.getElementById("menu1").style.display=(currState ? "block":"none");
-            document.getElementById("content2").style.display=(currState ? "none":"block");
-            }
-        );
+var currState = false;
+el.addEventListener("click", function(){currState = !currState;
+    var popup = document.getElementById("myForm");
+    popupStyle = window.getComputedStyle(popup),
+    displayS = popupStyle.display;
+    if (displayS == 'block') {document.getElementById("myForm").style.display = "none";}
+    el.style.backgroundImage=(currState ? "url(./assets/images/back.png)":"url(./assets/images/menu.png)");
+    // document.getElementById("body").style.backgroundColor=(currState ? "#ed6d1e":"white");
+    var temp1 = document.getElementsByClassName("content1");
+    for ( i = 0; i < temp1.length; i++ ) {
+        temp1[i].style.display=(currState ? "none":"block");
+    }
+    document.getElementById("menu1").style.display=(currState ? "block":"none");
+    if ($('#content2').length>0) {document.getElementById("content2").style.display=(currState ? "none":"block");}
+    if ($('#content3cards').length > 0) {document.getElementById("content3cards").style.display=(currState ? "none":"block");}
+    if ($('#numberList').length > 0) {document.getElementById("numberList").style.display=(currState ? "none":"block");}
+    if ($('#searchbar').length > 0) {document.getElementById("searchbar").style.display=(currState ? "none":"block");};} );
 
+// Changes color based on answer for survey
 var currStateChangeColor = false;
 function changeColor(value) {
     var tempEl2 = document.getElementsByClassName("w-100 answer");
     console.log(tempEl2.length+"\n");
-    for ( i = 0; i < tempEl2.length; i++ ) {
+    for ( let i = 0; i < tempEl2.length; i++ ) {
         console.log(tempEl2[i].style.backgroundColor + "\n");
     }
 }
